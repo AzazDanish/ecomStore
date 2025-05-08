8c578b4d2e0634efd3796901926b797ed5959688
Azaz Danish <azaz.danish7@gmail.com>
2025-05-09 00:43:00 +0530

    add loading to home page

---
+++ app/(routes)/loading.tsx
@@ -0,0 +1,8 @@
+import ProductPageSkeleton from "@/components/ProductCardSkeleton";
+import React from "react";
+
+const Loading = () => {
+  return <ProductPageSkeleton />;
+};
+
+export default Loading;
